2bdef9176319172fec3478bb608939a932b08465
Simon Fahrig <simon.fahrig@yahoo.com>
2022-04-02 22:05:48 +0200

    Add author to flora category

--- components/author.js
+++ components/author.js
@@ -4,17 +4,24 @@ import styles from './author.module.css'
 export default function Author({ authorString }) {
     return (
         <div>
+            <hr style={{ margin: "150px 0 0" }} />
             <h3>Erstellt von:</h3>
-            <div>
-                <Image className={styles.profileImage}
-                    priority
-                    src={"/images/authors/" + authorString + ".jpg"}
-                    height={64}
-                    width={64}
-                    alt={authorString}
-                />
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div>
+                    <Image className={styles.profileImage}
+                        priority
+                        src={"/images/authors/" + authorString + ".jpg"}
+                        height={64}
+                        width={64}
+                        alt={authorString}
+                    />
+                    <div></div>
+                </div>
+                <div style={{ marginLeft: "10px", display: "flex", flexDirection: "column" }}>
+                    <div className={styles.authorName}>{authorString}</div>
+                    <div>citation this is why the earth is turning round</div>
+                </div>
             </div>
-            <p>{authorString}</p>
-        </div>
+        </div >
     )
 }
